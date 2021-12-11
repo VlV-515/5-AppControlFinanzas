71b0310cde559b6f5903ebbf632066109f53e72b
mail <mail@mail.com>
2021-12-10 22:14:36 -0600

    Terminado de diseño de tabla, comienza proceso de logica

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { TableComponent } from './components/table/table.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    TableComponent
   ],
   imports: [
     CommonModule,
